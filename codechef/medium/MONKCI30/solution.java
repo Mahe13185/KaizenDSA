f9e5596b5e1a2069de9947840956fe7b7413780c
Mahe13185 <mahendragampala13185@gmail.com>
2026-08-02 20:27:21 +0530

    ✅ MONKCI30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-02

---
+++ codechef/medium/MONKCI30/solution.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Step 1: Take input for number of languages
+        int n = sc.nextInt();
+        sc.nextLine();
+        // Step 2: Create ArrayList to store language names
+        ArrayList<String> arr = new ArrayList<>();
+        // Step 3: Read n languages from user
+        
+        for(int i=0;i<n;i++){
+            arr.add(sc.nextLine());
+        }
+        // Step 4: Sort the list
+        Collections.sort(arr);
+        // Step 5: Print first and last elements
+        System.out.println("First Language:" + arr.get(0));
+        System.out.println("Last Language:" + arr.get(n-1));
+
+        
+        sc.close();
+    }
+}
