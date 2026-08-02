07caf60b741005362c038ea61efa639e36c1fe4b
Mahe13185 <mahendragampala13185@gmail.com>
2026-08-02 21:24:52 +0530

    ✅ VAL114 - Rating 318 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-08-02

---
+++ codechef/easy/VAL114---Rating-318/solution.cpp
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int x = sc.nextInt();
+	    System.out.println( (x == 121) ? "Likely" : "Unlikely");
+	}
+}
