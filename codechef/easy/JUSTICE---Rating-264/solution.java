6060a8d286bccaf2eb642ca9d1f05f1740f01791
Mahe13185 <mahendragampala13185@gmail.com>
2026-08-02 21:21:55 +0530

    ✅ JUSTICE - Rating 264 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-02

--- codechef/easy/JUSTICE---Rating-264/solution.java
+++ codechef/easy/JUSTICE---Rating-264/solution.java
@@ -9,7 +9,7 @@ class Codechef
 	    Scanner sc = new Scanner(System.in);
 	    int a = sc.nextInt();
 	    int b = sc.nextInt();
-	    System.out.println( (a > b) ? "Yes" : "No" );
+	    System.out.println( (a >= b) ? "Yes" : "No" );
 
 	}
 }
