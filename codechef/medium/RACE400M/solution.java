e0c8cd4352eff86ebee35336680d03b9fe69e44d
Mahe13185 <mahendragampala13185@gmail.com>
2026-08-30 16:51:15 +0530

    ✅ RACE400M | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-30

--- codechef/medium/RACE400M/solution.java
+++ codechef/medium/RACE400M/solution.java
@@ -5,12 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
         while (t-- > 0) {
-            int x = 400/scanner.nextInt();
-            int y = 400/scanner.nextInt();
-            int z = 400/scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int z = scanner.nextInt();
             
-            if(x > y && x > z) System.out.println("Alice");
-            else if(y > x && y > z) System.out.println("Bob");
+            if(x < y && x < z) System.out.println("Alice");
+            else if(y < x && y < z) System.out.println("Bob");
             else System.out.println("Charlie");
             
         }
