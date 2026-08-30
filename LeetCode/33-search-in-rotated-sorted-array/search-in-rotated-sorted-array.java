93300eb329f533e526504629bb1c65200ed3e9c9
Mahe13185 <mahendragampala13185@gmail.com>
2026-08-30 21:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (65.97%) - LeetSync

--- LeetCode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ LeetCode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,28 @@
 class Solution {
     public int search(int[] arr, int target) {
-        for(int i =0;i<arr.length;i++){
-            if(arr[i] == target){
-                return i;
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left<=right){
+            int middle = left + (right - left) / 2;
+            if(arr[middle] == target) return middle;
+
+            // Checks for left part is sorted or not 
+            if(arr[left] <= arr[middle]){
+                if(arr[left] <= target && target < arr[middle]){
+                    right = middle - 1;
+                }
+                else{
+                    left = middle + 1;
+                }
+            }else{
+                if(arr[middle] < target && target <= arr[right]){
+                    left = middle + 1;
+                }else{
+                    right = middle - 1;
+                }
             }
         }
         return -1;
+        } 
     }
-}
