6146fc54b50701bfe9b32fe8ff7979a06c108016
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-09-14 15:46:08 +0530

    updated and revised all the problem related to arrays ....

--- src/Revision/Arrays.java
+++ src/Revision/Arrays.java
@@ -1,5 +1,10 @@
 package Revision;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class Arrays {
     static int findMax(int[] arr){
         int max = Integer.MIN_VALUE;
@@ -74,7 +79,89 @@ public class Arrays {
         }
         return arr;
     }
+    static int[] makeZeros(int[] arr){
+        int index = 0;
+        for (int j =0;j<arr.length;j++){
+            if(arr[j] != 0){
+                arr[index] = arr[j];
+                index++;
+            }
+        }
+        for (int i =index;i<arr.length;i++){
+            arr[i]= 0;
+        }
+        return arr;
+    }
 
+    static boolean unionOfTwoSortedArray(int[] arr, int[] arr1){
+//        Set<Integer> set = new TreeSet<>();
+//        for (int ele : arr)
+//            set.add(ele);
+//        for (int ele : arr1) set.add(ele);
+//        System.out.println(set);
+//        return false;
+        int n = arr.length;
+        int m = arr1.length;
+        List<Integer> union = new ArrayList<>();
+        int i = 0, j=0;
+        while (i< n && j<m){
+            if (arr[i] < arr[j]){
+                if (union.isEmpty() || union.get(union.size()-1) != arr[i]){
+                    union.add(arr[i]);
+                }
+                i++;
+            }else if (arr[i] > arr[j]){
+                if (union.isEmpty() || union.get(union.size()-1) != arr[j]){
+                    union.add(arr[j]);
+                }
+                j++;
+            }else {
+                if (union.isEmpty() || union.get(union.size()-1) != arr[i]){
+                    union.add(arr[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        while (i<n){
+            if (union.isEmpty() || union.get(union.size()-1) != arr[i]){
+                union.add(arr[i]);
+            }
+            i++;
+        }
+        while (j<m){
+            if (union.isEmpty() || union.get(union.size()-1) != arr[j]){
+                union.add(arr[j]);
+            }
+            j++;
+        }
+        System.out.println(union);
+        return false;
+    }
+    static int[] twoSum(int[] arr , int target){
+//        HashMap<Integer,Integer> set = new HashMap<>();
+//        for(int i=0;i<arr.length;i++){
+//           int compliment = target - arr[i];
+//           if (set.containsKey(compliment)){
+//               return new int[]{set.get(compliment),i};
+//           }
+//           set.put(arr[i],i);
+//        }
+//        return new int[]{-1,-1};
+
+        int i =0;
+        int j = arr.length - 1;
+        while (i<j){
+            if(arr[i] + arr[j] > target){
+                j--;
+            } else if (arr[i] + arr[j] < target) {
+                i++;
+            } else if (arr[i] + arr[j] == target) {
+                return new int[]{i,j};
+            }
+        }
+        return new int[]{-1,-1};
+    }
     static void main() {
         int[] arr = {4,7,2,9,1};
         int[] arr1 = {1,2,3,4,5,6,7,8};
@@ -84,8 +171,23 @@ public class Arrays {
         System.out.println(findSum(arr));
         System.out.println(findSecoundMax(arr));
         System.out.println(findSecondMin(arr));
-        System.out.println(java.util.Arrays.toString(rotateArrayBy_N(arr1,3,"right")));
+
+        int[] arr112 = {1,2,3,4,5,6,7,8};
+        System.out.println(java.util.Arrays.toString(rotateArrayBy_N(arr112,3,"right")));
+        int[] arr2 = {1,2,3,4,5,6,7,8};
         System.out.println(java.util.Arrays.toString(rotateArrayBy_N(arr1,3,"left")));
 
+        int[] arr3 = {0,1,0,3,12};
+        System.out.println(java.util.Arrays.toString(makeZeros(arr3)));
+
+        System.out.println(unionOfTwoSortedArray(arr112,arr3));
+
+        int[] arr123 = {2, 7, 11, 15};
+        System.out.println(java.util.Arrays.toString(twoSum(arr123, 9)));
+
+
+        String str = new String("HelloWorld");
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse());
     }
 }
