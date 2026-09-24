d87538b445f574295430564a3c4269a459194a3f
Mahendra Gampala <mahendragampala13185@gmail.com>
2026-09-24 20:11:25 +0530

    Time: 1 ms (99.88%) | Memory: 63 MB (9.12%) - LeetSync

--- LeetCode/169-majority-element/majority-element.java
+++ LeetCode/169-majority-element/majority-element.java
@@ -1,14 +1,23 @@
 class Solution {
     public int majorityElement(int[] arr) {
         int n = arr.length;
-        long highCount = n / 2;
-        HashMap<Integer,Integer> seen = new HashMap<>();
+        // long highCount = n / 2;
+        // HashMap<Integer,Integer> seen = new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     seen.put(arr[i],seen.getOrDefault(arr[i],0) + 1);
+        //     if(seen.get(arr[i]) > highCount){
+        //         return arr[i];
+        //     }
+        // }
+        // return -1;
+
+        int count = 0;
+        int candidate = arr[0];
         for(int i=0;i<n;i++){
-            seen.put(arr[i],seen.getOrDefault(arr[i],0) + 1);
-            if(seen.get(arr[i]) > highCount){
-                return arr[i];
-            }
+            if(count == 0) candidate = arr[i];
+            if(arr[i] == candidate) count++;
+            else count--;
         }
-        return -1;
+        return candidate;
     }
 }
